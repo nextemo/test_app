59447eaf3e0626d9f29671be7fb37e6610bd5ab9
Jhoni Dewa <jhoni.dewa@gmail.com>
2019-12-20 09:19:30 +0100

    Future is the answer!

--- lib/main.dart
+++ lib/main.dart
@@ -4,116 +4,120 @@ import 'package:test_app/pages/hero.dart';
 import 'package:test_app/pages/test.dart';
 
 void main() => runApp(
-  OK(),
+      OK(),
     );
 
-class Test extends StatefulWidget {
-  @override
-  _TestState createState() => _TestState();
-}
-
-class _TestState extends State<Test> {
-  var url = "https://static.thenounproject.com/png/17241-200.png";
-  Widget buildList(AsyncSnapshot snapshot, int documentName, String fieldName) {
-    return SliverList(
-        delegate: SliverChildListDelegate([
-      Container(
-        margin: EdgeInsets.symmetric(vertical: 5.0),
-        color: Colors.blue,
-        child: ListTile(
-          title: Row(
-            children: <Widget>[
-              Expanded(
-                child: Text(snapshot.data[documentName][fieldName]),
-              ),
-              Expanded(
-                child: Text(snapshot.data[documentName][fieldName]),
-              ),
-            ],
-          ),
-          subtitle: Text("subName"),
-          leading: Container(
-            child: FittedBox(
-              fit: BoxFit.contain,
-              child: Image.network(url),
-            ),
-          )
-        ),
-      ),
-    ]));
-  }
-
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          leading: Icon(Icons.home),
-          title: Text("just a test"),
-        ),
-        body: Hero(
-          tag: "Heroen",
-          child: StreamBuilder(
-              stream: Firestore.instance.collection("cars").snapshots(),
-              builder: (context, snapshot) {
-                if (snapshot.hasError){
-                  return Center(
-                      child: Text("there's an error, maybe you should turn on your wifi")
-                  );
-                } else if (!snapshot.hasData) {
-                  return Center(
-                      child: Container(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            CircularProgressIndicator(),
-                            SizedBox(height: 20.0,),
-                            Text("Please wait...")
-                          ],
-                        ),
-                      )
-                  );
-                }
-                return CustomScrollView(
-                  scrollDirection: Axis.vertical,
-                  slivers: <Widget>[
-                    SliverAppBar(
-                      pinned: true,
-                      title: Text("Another TEST",
-                        style: TextStyle(fontSize: 30.0, color: Colors.white),
-                      ),
-                      expandedHeight: 200.0,
-                      flexibleSpace: FlexibleSpaceBar(),
-                    ),
-                    buildList(snapshot, 0, "brand"),
-                    buildList(snapshot, 0, "brand"),
-                    buildList(snapshot, 0, "brand"),
-                    buildList(snapshot, 0, "brand"),
-                    buildList(snapshot, 0, "brand"),
-                    buildList(snapshot, 0, "brand"),
-                    buildList(snapshot, 0, "brand"),
-                    buildList(snapshot, 0, "brand"),
-                    buildList(snapshot, 0, "brand"),
-                    buildList(snapshot, 0, "brand"),
-                    buildList(snapshot, 0, "brand"),
-                    buildList(snapshot, 1, "brand"),
-                    buildList(snapshot, 1, "brand"),
-                    buildList(snapshot, 1, "brand"),
-                    buildList(snapshot, 1, "brand"),
-                    buildList(snapshot, 1, "brand"),
-                    buildList(snapshot, 1, "brand"),
-                    buildList(snapshot, 1, "brand"),
-                    buildList(snapshot, 1, "brand"),
-                    buildList(snapshot, 1, "brand"),
-                    buildList(snapshot, 1, "brand"),
-                    buildList(snapshot, 1, "brand"),
-                  ],
-                );
-              }),
-        ),
-      ),
-    );
-  }
-}
+//class Test extends StatefulWidget {
+//  @override
+//  _TestState createState() => _TestState();
+//}
+//
+//class _TestState extends State<Test> {
+//
+//  void getStream() async {
+//    final fire = Firestore.instance;
+//    QuerySnapshot aSnap = await fire.collection("cars").getDocuments();
+//    var ok = aSnap.documents;
+//  }
+//
+//  var url = "https://static.thenounproject.com/png/17241-200.png";
+//  Widget buildList(AsyncSnapshot snapshot, int documentName, String fieldName) {
+//    return SliverList(
+//        delegate: SliverChildListDelegate([
+//      Container(
+//        margin: EdgeInsets.symmetric(vertical: 5.0),
+//        color: Colors.blue,
+//        child: ListTile(
+//          title: Row(
+//            children: <Widget>[
+//              Expanded(
+//                child: Text(snapshot.data.documents[documentName][fieldName]),
+//              ),
+//              Expanded(
+//                child: Text(snapshot.data.documents[documentName][fieldName]),
+//              ),
+//            ],
+//          ),
+//          subtitle: Text("subName"),
+//          leading: Container(
+//            child: Icon(Icons.directions_car, size: 25.5,),
+//          )
+//        ),
+//      ),
+//    ]));
+//  }
+//
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    return MaterialApp(
+//      home: Scaffold(
+//        appBar: AppBar(
+//          leading: Icon(Icons.home),
+//          title: Text("just a test"),
+//        ),
+//        body: Hero(
+//          tag: "Heroen",
+//          child: StreamBuilder(
+//              stream: getStream(),
+//              builder: (context, snapshot) {
+////                if (snapshot.hasError){
+////                  return Center(
+////                      child: Text("there's an error, maybe you should turn on your wifi")
+////                  );
+////                } else if (!snapshot.hasData) {
+////                  return Center(
+////                      child: Container(
+////                        child: Column(
+////                          mainAxisAlignment: MainAxisAlignment.center,
+////                          children: <Widget>[
+////                            CircularProgressIndicator(),
+////                            SizedBox(height: 20.0,),
+////                            Text("Please wait...")
+////                          ],
+////                        ),
+////                      )
+////                  );
+////                }
+//                return CustomScrollView(
+//                  scrollDirection: Axis.vertical,
+//                  slivers: <Widget>[
+//                    SliverAppBar(
+//                      pinned: true,
+//                      title: Text("Another TEST",
+//                        style: TextStyle(fontSize: 30.0, color: Colors.white),
+//                      ),
+//                      expandedHeight: 200.0,
+//                      flexibleSpace: FlexibleSpaceBar(),
+//                    ),
+//                    buildList(snapshot, 0, "brand"),
+//                    buildList(snapshot, 0, "brand"),
+//                    buildList(snapshot, 0, "brand"),
+//                    buildList(snapshot, 0, "brand"),
+//                    buildList(snapshot, 0, "brand"),
+//                    buildList(snapshot, 0, "brand"),
+//                    buildList(snapshot, 0, "brand"),
+//                    buildList(snapshot, 0, "brand"),
+//                    buildList(snapshot, 0, "brand"),
+//                    buildList(snapshot, 0, "brand"),
+//                    buildList(snapshot, 0, "brand"),
+//                    buildList(snapshot, 1, "brand"),
+//                    buildList(snapshot, 1, "brand"),
+//                    buildList(snapshot, 1, "brand"),
+//                    buildList(snapshot, 1, "brand"),
+//                    buildList(snapshot, 1, "brand"),
+//                    buildList(snapshot, 1, "brand"),
+//                    buildList(snapshot, 1, "brand"),
+//                    buildList(snapshot, 1, "brand"),
+//                    buildList(snapshot, 1, "brand"),
+//                    buildList(snapshot, 1, "brand"),
+//                    buildList(snapshot, 1, "brand"),
+//                  ],
+//                );
+//              }),
+//        ),
+//      ),
+//    );
+//  }
+//}
 
--- lib/pages/test.dart
+++ lib/pages/test.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 class OK extends StatefulWidget {
   @override
@@ -8,70 +9,115 @@ class OK extends StatefulWidget {
 
 class _OKState extends State<OK> {
   var url = "https://static.thenounproject.com/png/17241-200.png";
-  var car = "";
-  final storefire = Firestore.instance;
-  void getStream()async{
-    final stream = await storefire.collection("cars").getDocuments();
-    if(stream.documents != null){
-      print(stream.documents[0]["brand"]);
-      print(stream.documents[0]["type"]);
-      print(stream.documents[1]["brand"]);
-      print(stream.documents[1]["type"]);
-      print(stream.documents[2]["brand"]);
-      print(stream.documents[2]["type"]);
-      print(stream.documents[3]["brand"]);
-      print(stream.documents[3]["type"]);
-      setState(() {
-        car = stream.documents[0]["brand"];
-      });
 
-    }
+  Future getFuture() async {
+    final storefire = Firestore.instance;
+    QuerySnapshot qSnaps = await storefire.collection("cars").getDocuments();
+    return qSnaps.documents;
   }
 
   Widget buildList(AsyncSnapshot snapshot, int documentName, String fieldName) {
     return SliverList(
         delegate: SliverChildListDelegate([
-          Container(
-            margin: EdgeInsets.symmetric(vertical: 5.0),
-            color: Colors.blue,
-            child: ListTile(
-                title: Row(
-                  children: <Widget>[
-                    Expanded(
-                      child: Text(snapshot.data[documentName][fieldName]),
-                    ),
-                    Expanded(
-                      child: Text(snapshot.data[documentName][fieldName]),
-                    ),
-                  ],
+      Container(
+        margin: EdgeInsets.symmetric(vertical: 5.0),
+        color: Colors.blue,
+        child: ListTile(
+            title: Row(
+              children: <Widget>[
+                Expanded(
+                  child: Text(snapshot.data[documentName][fieldName]),
                 ),
-                subtitle: Text("subName"),
-                leading: Container(
-                  child: FittedBox(
-                    fit: BoxFit.contain,
-                    child: Image.network(url),
-                  ),
-                )
+                Expanded(
+                  child: Text(snapshot.data[documentName][fieldName]),
+                ),
+              ],
             ),
-          ),
-        ]));
+            subtitle: Text("subName"),
+            leading: Container(
+              child: FittedBox(
+                fit: BoxFit.contain,
+                child: Image.network(url),
+              ),
+            )),
+      ),
+    ]));
   }
 
+  Widget detail() {
+    return MaterialApp(
+      home: Container(
+        color: Colors.blue,
+        child: Center(
+          child: Text(
+            "Hi there",
+            style: TextStyle(fontSize: 40.0, color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildText(
+      AsyncSnapshot snapshot, int docName, String brandName, String type) {
+    return Container(
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+//            Text(snapshot.data[docName][fldName]),
+//            SizedBox(height: 10,)
+            ListTile(
+              onTap: ()=>
+          detail(),
+//                  Navigator.push(context, MaterialPageRoute(builder: (context)=> Detail())),
+              leading:Icon(Icons.directions_car),
+              title: Text(snapshot.data[docName][brandName]),
+              subtitle: Text(snapshot.data[docName][type]),
+            )
+          ],
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: CustomScrollView(
-        scrollDirection: Axis.vertical,
-        slivers: <Widget>[
-          SliverAppBar(
-            expandedHeight: 150,
-            title: Text("Hi there"),
+        home: Scaffold(
+      body: FutureBuilder(
+          future: getFuture(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting)
+              return CircularProgressIndicator();
+            else {
+              return Container(
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[buildText(snapshot, 0, "brand", "type")],
+                  ),
+                ),
+              );
+            }
+          }),
+    ));
+  }
+}
+
+class Detail extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Container(
+        color: Colors.blue,
+        child: Center(
+          child: Text(
+            "Hi there",
+            style: TextStyle(fontSize: 40.0, color: Colors.white),
           ),
-
-
-        ],
-      )
+        ),
+      ),
     );
   }
 }
